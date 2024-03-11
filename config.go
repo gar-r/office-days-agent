619d6781bb479c3a05061c6eb01fc9f71ed2955a
Richard Garai <richard.garai.con@hotstar.com>
2024-03-11 12:14:34 +0100

    change default poll interval

--- config.go
+++ config.go
@@ -5,7 +5,7 @@ const EnvConfigPrefix = "OFFICE_DAYS"
 
 type Conf struct {
 	DataPath            string `split_words:"true" default:"office-days.dat"`
-	PollIntervalSeconds int    `split_words:"true" default:"60"`
+	PollIntervalSeconds int    `split_words:"true" default:"1800"`
 	WifiName            string `split_words:"true" required:"true"`
 	WifiDeviceName      string `split_words:"true" default:"en0"`
 }
