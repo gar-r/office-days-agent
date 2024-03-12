31bf33f02533cf3d5d85465026d97d308ad0a22a
Richard Garai <richard.garai.con@hotstar.com>
2024-03-12 11:04:50 +0100

    fix db path defaults

--- config.go
+++ config.go
@@ -5,7 +5,7 @@ const EnvConfigPrefix = "OFFICE_DAYS"
 
 type Conf struct {
 	ApiServerPort       int    `split_words:"true" default:"23460"`
-	DBPath              string `split_words:"true" default:"$HOME/.config/office-days-agent/db"`
+	DBPath              string `split_words:"true" default:"/usr/local/office-days-agent/db"`
 	PollIntervalSeconds int    `split_words:"true" default:"1800"`
 	WifiName            string `split_words:"true" required:"true"`
 	WifiDeviceName      string `split_words:"true" default:"en0"`
